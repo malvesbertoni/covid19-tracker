dcc5b789f16a1b4e334cd6f361c54a6d45069c73
malvesbertoni <malvesbertoni@gmail.com>
2020-05-05 14:31:59 -0300

    app's sketch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Herou wordi</h1>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+  
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
