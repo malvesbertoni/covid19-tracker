8a7d104d199019d78bf621792bb69d8a7f4bad58
malvesbertoni <malvesbertoni@gmail.com>
2020-05-06 14:48:20 -0300

    bug fixed

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -19,7 +19,7 @@ function CountryPicker({ handleCountryChange }) {
     return(
         <FormControl>
             <NativeSelect default="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="global">Global</option>
+                <option value="">Global</option>
                 {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
